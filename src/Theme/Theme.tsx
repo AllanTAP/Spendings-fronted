f09f14d5437d4784d2ba139a95f4e8a07c196875
Allan Amaral <allan.depontes@gmail.com>
2023-12-17 20:16:14 +0000

    move old version to folder

--- src/Theme/Theme.tsx
+++
@@ -1,8 +0,0 @@
-const theme = (isDark: boolean) => ({
-  white: isDark ? '#FFF' : '#000',
-  darkBlue: '#33758F',
-  normalBlue: '#44B4CC',
-  lightBlue: '#52D2D9'
-})
-
-export default theme
--- src/components/Header/styles.tsx
+++
@@ -1,8 +0,0 @@
-/* use-strict */
-import styled from 'styled-components'
-// import { Layout } from 'antd'
-
-export const StyledHeader = styled.div`
-  background: #606060;
-  height: 60px;
-`
